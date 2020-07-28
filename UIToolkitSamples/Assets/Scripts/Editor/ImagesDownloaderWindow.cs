8ae424effddc4ab9a285725904ba81331bda69ce
Rémi Carreira <remi.carreira@muchobestostudio.fr>
2020-07-28 23:34:38 +0200

    [wip] images downloader UI [add] editor coroutines packages

--- UIToolkitSamples/Assets/Scripts/Editor/ImagesDownloaderWindow.cs
+++ UIToolkitSamples/Assets/Scripts/Editor/ImagesDownloaderWindow.cs
@@ -1,12 +1,31 @@
-﻿using UnityEngine;
+﻿using Unity.EditorCoroutines.Editor;
+using UnityEngine;
 using UnityEditor;
+using UnityEngine.Networking;
 using UnityEngine.UIElements;
+using System.Collections;
+using UnityEditor.UIElements;
+using System.IO;
+using System.Collections.Generic;
 
 public class ImagesDownloaderWindow : EditorWindow
 {
 	#region Variables
 
 	private static VisualTreeAsset _uxml = null;
+
+	private static EditorCoroutine _downloadCoroutine = null;
+
+	private Button _downloadButton = null;
+	private Button _saveButton = null;
+	private TextField _urlField = null;
+	private ObjectField _textureField = null;
+
+	private ProgressBar _progressBar = null;
+	private Label _progressBarLabel = null;
+	private VisualElement _progressBarProgression = null;
+
+	private Texture2D _downloadedTexture = null;
 
 	#endregion
 
@@ -26,7 +45,7 @@ public class ImagesDownloaderWindow : EditorWindow
 		{
 			_uxml.CloneTree(window.rootVisualElement);
 
-			LinkToUIElements(window.rootVisualElement);
+			window.Initialize();
 		}
 		else
 		{
@@ -36,12 +55,102 @@ public class ImagesDownloaderWindow : EditorWindow
 		window.Show();
 	}
 
-	private static void LinkToUIElements(VisualElement root)
-	{
-		TextField urlField = root.Q<TextField>("Url");
-		Button downloadButton = root.Q<Button>("Download");
+	#endregion
 
-		downloadButton.clicked += () => { Debug.Log("Download button clicked"); };
+	#region Initialize / OnDestroy
+
+	public void Initialize()
+	{
+		// Note (rémi): Retrieve UIElements
+		_urlField = rootVisualElement.Q<TextField>("Url");
+		_downloadButton = rootVisualElement.Q<Button>("Download");
+		_progressBar = rootVisualElement.Q<ProgressBar>("DownloadProgression");
+		_textureField = rootVisualElement.Q<ObjectField>("Result");
+		_saveButton = rootVisualElement.Q<Button>("Save");
+
+		// Note (rémi): Retrieve UIElements children
+		_progressBarLabel = _progressBar.Q<Label>();
+		// Remarks (rémi): remove the dot at beginning when search using class name
+		_progressBarProgression = _progressBar.Q<VisualElement>(className: "unity-progress-bar__progress");
+
+		// Note (rémi): Link UIElements
+		_downloadButton.clicked += OnDownloadButtonClicked;
+		_saveButton.clicked += OnSaveButtonClicked;
+	}
+
+	private void OnDestroy()
+	{
+		 /// ToDo (remi): Dispose request and coroutine
+	}
+
+	private void OnDownloadButtonClicked()
+	{
+		_downloadCoroutine = EditorCoroutineUtility.StartCoroutine(DownloadTexture(_urlField.text), this);
+	}
+
+	private IEnumerator	DownloadTexture(string url)
+	{
+		_downloadButton.SetEnabled(false);
+
+		_progressBar.style.display = DisplayStyle.Flex;
+		_progressBarLabel.style.color = Color.black;
+		_progressBarProgression.style.unityBackgroundImageTintColor = Color.blue;
+
+		_textureField.style.display = DisplayStyle.None;
+		_saveButton.style.display = DisplayStyle.None;
+
+		using (UnityWebRequest textureRequest = UnityWebRequestTexture.GetTexture(url))
+		{
+			UnityWebRequestAsyncOperation operation = textureRequest.SendWebRequest();
+
+			while (!textureRequest.isDone)
+			{
+				yield return null;
+
+				_progressBar.SetValueWithoutNotify(operation.progress);
+				_progressBar.title = _progressBar.value.ToString() + "%";
+			}
+
+			if (textureRequest.isHttpError == false && textureRequest.isNetworkError == false)
+			{
+				_downloadedTexture = DownloadHandlerTexture.GetContent(textureRequest);
+
+				_textureField.style.display = DisplayStyle.Flex;
+				_saveButton.style.display = DisplayStyle.Flex;
+
+				_progressBar.title = "Done";
+			}
+			else
+			{
+				_downloadedTexture = null;
+
+				_progressBarLabel.style.color = Color.white;
+				_progressBarProgression.style.unityBackgroundImageTintColor = Color.red;
+
+
+				_progressBar.SetValueWithoutNotify(100f);
+				_progressBar.title = "Error";
+			}
+		}
+
+		_downloadButton.SetEnabled(true);
+	}
+
+	private void OnSaveButtonClicked()
+	{
+		string path = EditorUtility.SaveFilePanelInProject("Save Texture", "Image", "png", "Please enter a file name to save the texture to");
+
+		if (string.IsNullOrEmpty(path) == false)
+		{
+			byte[] pngData = _downloadedTexture.EncodeToPNG();
+			if (pngData != null)
+			{
+				File.WriteAllBytes(path, pngData);
+
+				// As we are saving to the asset folder, tell Unity to scan for modified or new assets
+				AssetDatabase.Refresh();
+			}
+		}
 	}
 
 	#endregion
